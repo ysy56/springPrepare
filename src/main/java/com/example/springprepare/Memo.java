d8bc1bea270e1e6c6523a6272b5c1c08898b86fa
moony <78634780+ysy56@users.noreply.github.com>
2024-05-13 16:41:32 +0900

    [1-7] Lombok과 application.properties

---
+++ src/main/java/com/example/springprepare/Memo.java
@@ -0,0 +1,11 @@
+package com.example.springprepare;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Memo {
+    private String username;
+    private String contents;
+}
