04136557c176bb23693a8ba66d413a2aecc5499a
moony <78634780+ysy56@users.noreply.github.com>
2024-05-13 18:09:49 +0900

    [1-14] Path Variable과 Request Param

---
+++ src/main/java/com/example/springprepare/request/RequestController.java
@@ -0,0 +1,42 @@
+package com.sparta.springmvc.request;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/hello/request")
+public class RequestController {
+    @GetMapping("/form/html")
+    public String helloForm() {
+        return "hello-request-form";
+    }
+
+    // [Request sample]
+    // GET http://localhost:8080/hello/request/star/Robbie/age/95
+    @GetMapping("/star/{name}/age/{age}")
+    @ResponseBody
+    public String helloRequestPath(@PathVariable String name, @PathVariable int age)
+    {
+        return String.format("Hello, @PathVariable.<br> name = %s, age = %d", name, age);
+    }
+
+    // [Request sample]
+    // GET http://localhost:8080/hello/request/form/param?name=Robbie&age=95
+    @GetMapping("/form/param")
+    @ResponseBody
+    public String helloGetRequestParam(@RequestParam(required = false) String name, int age) {
+        return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
+    }
+
+    // [Request sample]
+    // POST http://localhost:8080/hello/request/form/param
+    // Header
+    //  Content type: application/x-www-form-urlencoded
+    // Body
+    //  name=Robbie&age=95
+    @PostMapping("/form/param")
+    @ResponseBody
+    public String helloPostRequestParam(@RequestParam String name, @RequestParam int age) {
+        return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
+    }
+}
