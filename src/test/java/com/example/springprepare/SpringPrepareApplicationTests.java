5cfd594f9f0fb0aacae3c89b7350ad458ca3452d
moony <78634780+ysy56@users.noreply.github.com>
2024-05-13 12:10:15 +0900

    init commit

---
+++ src/test/java/com/example/springprepare/SpringPrepareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springprepare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPrepareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
