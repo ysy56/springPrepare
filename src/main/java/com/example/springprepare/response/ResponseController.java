559507f7ca337ec2e0f8c89fc2d5be0f475abdae
moony <78634780+ysy56@users.noreply.github.com>
2024-05-13 17:39:18 +0900

    [1-12] 데이터를 Client에 반환하는 방법

---
+++ src/main/java/com/example/springprepare/response/ResponseController.java
@@ -0,0 +1,30 @@
+package com.example.springprepare.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/response")
+public class ResponseController {
+    // [Response header]
+    //   Content-Type: text/html
+    // [Response body]
+    //   {"name":"Robbie","age":95}
+    @GetMapping("/json/string")
+    @ResponseBody
+    public String helloStringJson() {
+        return "{\"name\":\"Robbie\",\"age\":95}";
+    }
+
+    // [Response header]
+    //   Content-Type: application/json
+    // [Response body]
+    //   {"name":"Robbie","age":95}
+    @GetMapping("/json/class")
+    @ResponseBody
+    public Star helloClassJson() {
+        return new Star("Robbie", 95);
+    }
+}
---
+++ src/main/java/com/example/springprepare/response/Star.java
@@ -0,0 +1,16 @@
+package com.example.springprepare.response;
+
+import lombok.Getter;
+
+@Getter
+public class Star {
+    String name;
+    int age;
+
+    public Star(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Star() {}
+}
