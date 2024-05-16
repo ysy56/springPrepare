0c4a32959343cdb7b13b29170c4b4d93a1d2ef5f
moony <78634780+ysy56@users.noreply.github.com>
2024-05-16 16:55:14 +0900

    [2-7] Entity 이해하기

--- src/main/java/com/example/springprepare/entity/Memo.java
+++ src/main/java/com/example/springprepare/entity/Memo.java
@@ -23,3 +23,20 @@ public class Memo {
         this.contents=requestDto.getContents();
     }
 }
+
+//@Entity // JPA가 관리할 수 있는 Entity 클래스 지정
+//@Table(name = "memo") // 매핑할 테이블의 이름을 지정
+//public class Memo {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment
+//    private Long id;
+//
+//    // nullable: null 허용 여부
+//    // unique: 중복 허용 여부 (false 일때 중복 허용)
+//    @Column(name = "username", nullable = false, unique = true)
+//    private String username;
+//
+//    // length: 컬럼 길이 지정
+//    @Column(name = "contents", nullable = false, length = 500)
+//    private String contents;
+//}
