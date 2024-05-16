0011e5cd4cd7851f255a5f896655242ef5ff593c
moony <78634780+ysy56@users.noreply.github.com>
2024-05-17 00:44:37 +0900

    [2-14] Query Methods란 무엇일까?

--- src/main/java/com/example/springprepare/repository/MemoRepository.java
+++ src/main/java/com/example/springprepare/repository/MemoRepository.java
@@ -4,7 +4,10 @@ import com.example.springprepare.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MemoRepository extends JpaRepository<Memo, Long> {
-
+    List<Memo> findAllByOrderByModifiedAtDesc();
+    List<Memo> findAllByUsername(String username);
 }
